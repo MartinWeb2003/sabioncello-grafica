36ed6f906fd5e4aceefc632a162ab29802237e10
MartinWeb2003 <144782669+MartinWeb2003@users.noreply.github.com>
2026-04-18 09:54:21 +0200

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/sabioncello-grafica/',
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
